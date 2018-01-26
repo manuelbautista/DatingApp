d52f9be7e508b9f0a7fc1fb80f6c910bab07fc1b
Manuel Bautista <b0hcoder@gmail.com>
2018-01-26 13:53:34 -0400

    Server Authentication Full Added

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -7,6 +7,7 @@ using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
@@ -15,61 +16,69 @@ namespace DatingApp.API.Controllers
     public class AuthController : Controller
     {
         private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository repo)
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            this._config = config;
             this._repo = repo;
         }
 
         [HttpPost("register")] //http://localhost:5000/api/auth/register
         //[FromBody] es para buscar la informacion dentro del body
-        public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto) {
-            
+        public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
+        {
 
-            if(await _repo.UserExists(userForRegisterDto.Username)){
-                ModelState.AddModelError("Username","User already exists");
+
+            if (await _repo.UserExists(userForRegisterDto.Username))
+            {
+                ModelState.AddModelError("Username", "User already exists");
             }
 
             //validate request  
-            if(!ModelState.IsValid) {
+            if (!ModelState.IsValid)
+            {
                 return BadRequest(ModelState);
             }
 
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            var userToCreate = new User { 
+            var userToCreate = new User
+            {
                 Username = userForRegisterDto.Username
             };
-            
+
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
         }
 
         [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto) {
+        public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto)
+        {
             var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
-            
-            if(userFromRepo == null)
+
+            if (userFromRepo == null)
                 return Unauthorized();
 
             //generate token
             var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes("super secret key");
-            var tokenDescriptor = new SecurityTokenDescriptor 
+            var key = Encoding.ASCII.GetBytes(this._config.GetSection("AppSettings:Token").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
                     new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
                     new Claim(ClaimTypes.Name, userFromRepo.Username)
                 }),
+                // Issuer = "",
                 Expires = DateTime.Now.AddDays(1),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
             };
             var token = tokenHandler.CreateToken(tokenDescriptor);
             var tokenString = tokenHandler.WriteToken(token);
 
-            return Ok(new {tokenString});
+            return Ok(new { tokenString });
         }
     }
 }
--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
@@ -17,6 +19,7 @@ namespace DatingApp.API.Controllers
             this._context = context;
 
         }
+        // [AllowAnonymous]
         // GET api/values
         [HttpGet] 
         public async Task<IActionResult> GetValues()
