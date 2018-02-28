7fc447094f927ed96f09d2e381f87cedd3bfc054
Manuel Bautista <b0hcoder@gmail.com>
2018-02-28 07:27:55 -0400

    Register form

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -47,14 +47,12 @@ namespace DatingApp.API.Controllers
             if (!string.IsNullOrEmpty(userForRegisterDto.Username))
                 userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            var userToCreate = new User
-            {
-                Username = userForRegisterDto.Username
-            };
+            var userToCreate = _mapper.Map<User>(userForRegisterDto);
 
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+            var userToReturn = _mapper.Map<UserForDetailedDto>(createdUser);
 
-            return StatusCode(201);
+            return CreatedAtRoute("GetUser", new {controller= "Users", id = createdUser.Id}, userToReturn);
         }
 
         [HttpPost("login")]
--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -29,6 +29,8 @@ namespace DatingApp.API.Helpers
 
              CreateMap<PhotoForCreationDto, Photo>();
             CreateMap<Photo, PhotoForReturnDto>();
+
+            CreateMap<UserForRegisterDto, User>();
             
         }
     }
